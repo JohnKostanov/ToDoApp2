e93e1ab1ceaf80862ccc9fb842838dda85d17229
JohnKostanov <kostanovd@icloud.com>
2020-01-01 14:43:39 +0100

    Add func testCellHasLocationLabel, func testCellHasLocationLabelInContentView(), func testCellHasDateLabel() and func testCellHasDateLabelInContentView()

--- ToDoAppTests/Controller/TaskCell.swift
+++ ToDoAppTests/Controller/TaskCell.swift
@@ -11,6 +11,8 @@ import UIKit
 class TaskCell: UITableViewCell {
     
     @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
     
     func configure(withTask task: Task) {
         
