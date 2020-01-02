197d70e6b4c26c854852d3d4f26d975c1d2214ab
JohnKostanov <kostanovd@icloud.com>
2020-01-03 00:52:11 +0100

    Add func testSaveUsesGeocoderToConvertCoorfinateFromAdress() and func testSaveButtonHasSaveMethod()

--- ToDoAppTests/Controller/NewTaskViewControllerTests.swift
+++ ToDoAppTests/Controller/NewTaskViewControllerTests.swift
@@ -8,10 +8,12 @@
 
 import XCTest
 @testable import ToDoApp
+import CoreLocation
 
 class NewTaskViewControllerTests: XCTestCase {
     
     var sut: NewTaskViewController!
+    var placemark: MockCLPlacemark!
     
     override func setUp() {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
@@ -50,6 +52,64 @@ class NewTaskViewControllerTests: XCTestCase {
     func testHasCancelButton() {
         XCTAssertTrue(sut.cancelButton.isDescendant(of: sut.view))
     }
+    
+    func testSaveUsesGeocoderToConvertCoorfinateFromAdress() {
+        let df = DateFormatter()
+        df.dateFormat = "dd.MM.yy"
+        let date = df.date(from: "01.01.20")
+        
+        sut.titleTextField.text = "Foo"
+        sut.locationTextField.text = "Bar"
+        sut.dateTextField.text = "01.01.20"
+        sut.adressTextField.text = "Huelva"
+        sut.descriptionTextField.text = "Baz"
+        
+        sut.taskManager = TaskManager()
+        let mockGeocoder = MockCLGeocoder()
+        sut.geocoder = mockGeocoder
+        sut.save()
+        
+        let coordinate = CLLocationCoordinate2D(latitude: 37.2586074, longitude: -6.9499692)
+        let location = Location(name: "Bar", coordinate: coordinate)
+        let generatedTask = Task(title: "Foo", description: "Baz", date: date, location: location)
+        
+        placemark = MockCLPlacemark()
+        placemark.mockCoordinate = coordinate
+        mockGeocoder.completionHandler?([placemark], nil)
+        
+        let task = sut.taskManager.task(at: 0)
+        
+        XCTAssertEqual(task, generatedTask)
+    }
+    
+    func testSaveButtonHasSaveMethod() {
+        let saveButton = sut.saveButton
+        
+        guard let actions = saveButton?.actions(forTarget: sut, forControlEvent: .touchUpInside) else {
+            XCTFail()
+            return
+        }
+        
+        XCTAssertTrue(actions.contains("save"))
+    }
+}
 
-
+extension NewTaskViewControllerTests {
+    class MockCLGeocoder: CLGeocoder {
+        
+        var completionHandler: CLGeocodeCompletionHandler?
+        
+        override func geocodeAddressString(_ addressString: String, completionHandler: @escaping CLGeocodeCompletionHandler) {
+            self.completionHandler = completionHandler
+        }
+    }
+    
+    class MockCLPlacemark: CLPlacemark {
+        
+        var mockCoordinate: CLLocationCoordinate2D!
+        
+        override var location: CLLocation? {
+            return CLLocation(latitude: mockCoordinate.latitude, longitude: mockCoordinate.longitude)
+        }
+    }
 }
