e0e7bbdceab6f87569e1d4071ab64ee1155442c0
JohnKostanov <kostanovd@icloud.com>
2020-01-10 22:05:47 +0100

    Add func testCanBeCreatedFromPlistDictionary()

--- ToDoAppTests/Model/TaskTests.swift
+++ ToDoAppTests/Model/TaskTests.swift
@@ -46,4 +46,19 @@ class TaskTests: XCTestCase {
         let task = Task(title: "Bar", description: "Baz", location: location)
         XCTAssertEqual(location, task.location)
     }
+    
+    func testCanBeCreatedFromPlistDictionary() {
+        let location = Location(name: "Baz")
+        let date = Date(timeIntervalSince1970: 10)
+        let task = Task(title: "Foo", description: "Bar", date: date, location: location)
+        
+        let locationDictionary: [String : Any] = ["name" : "Baz"]
+        let dictionary: [String : Any] = ["title" : "Foo",
+                                          "description" : "Bar",
+                                          "date" : date,
+                                          "location" : locationDictionary]
+        let createdTask = Task(dict: dictionary)
+        
+        XCTAssertEqual(task, createdTask)
+    }
 }
