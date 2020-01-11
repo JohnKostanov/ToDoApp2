83649b6a89fd8310d364d683c039bc2e8d32af39
JohnKostanov <kostanovd@icloud.com>
2020-01-11 17:37:54 +0100

    Add fix bugs

--- ToDoApp/Controller/DataProvider.swift
+++ ToDoApp/Controller/DataProvider.swift
@@ -68,7 +68,7 @@ extension DataProvider: UITableViewDataSource {
         case .done: task = taskManager.doneTask(at: indexPath.row)
         }
         
-        cell.configure(withTask: task)
+        cell.configure(withTask: task, done: task.isDone)
         
         return cell
     }
